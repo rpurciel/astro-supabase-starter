74facf9ef4863545df3c394b94b9b74de7464210
RPurciel <rpurciel@gmail.com>
2025-04-17 12:56:44 -0700

    Test

--- astro.config.ts
+++ astro.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: 'github-light-high-contrast',
-    },
-  },
-  vite: {
-    plugins: [tailwindcss()],
-    server: {
-      allowedHosts: ['.netlify.app']
-    }
-  },
-  adapter: netlify()
-});
-
 import { defineStackbitConfig } from "@stackbit/types";
 import { ContentfulContentSource } from "@stackbit/cms-contentful";
+
+// export default defineConfig({
+//   markdown: {
+//     shikiConfig: {
+//       theme: 'github-light-high-contrast',
+//     },
+//   },
+//   vite: {
+//     plugins: [tailwindcss()],
+//     server: {
+//       allowedHosts: ['.netlify.app']
+//     }
+//   },
+//   adapter: netlify()
+// });
 
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
